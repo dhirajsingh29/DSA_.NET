2e1d59e89dad1587bfd7555f630f22ab4934dd41
dhiraj_slt <dhiraj.kumar@sevenlakes.com>
2022-09-17 00:21:35 +0530

    Leetcode - 1431 | Kids With largest Num Of Candies

--- DSA_VS_Code/Program.cs
+++ DSA_VS_Code/Program.cs
@@ -23,7 +23,12 @@ int[] nums = new int[] {2,5,1,3,4,7};
 int n = 3;
 // Utility.PrintArray(ShuffleArray.Shuffle_1(nums, n));
 // ShuffleArray.Shuffle_2(nums, n);
-ShuffleArray.Shuffle_3(nums, n);
-Utility.PrintArray(nums);
+// ShuffleArray.Shuffle_3(nums, n);
+// Utility.PrintArray(nums);
+
+int[] candies = new int[] {2,3,5,1,3};
+int extraCandies = 3;
+IList<bool> res = KidsWithGreatestCandies.KidsWithCandies(candies, extraCandies);
+Utility.PrintListWithIndex<bool>(res);
 
 #endregion
--- DSA_VS_Code/Util.cs
+++ DSA_VS_Code/Util.cs
@@ -1,17 +1,35 @@
 public static class Utility
 {
-    public static void PrintArray (int[] nums)
+    public static void PrintArray<T> (T[] items)
     {
         Console.WriteLine($"index\tnums");
 
-        foreach (var (num, index) in nums.WithIndex())
+        foreach (var (item, index) in items.WithIndex())
         {
-            Console.WriteLine($"{index}\t{num}");
+            Console.WriteLine($"{index}\t{item}");
         }
     }
 
-    public static void Print<T>(string message, T item)
+    public static void Print<T> (string message, T item)
     {
         Console.WriteLine($"{message}: {item}");
+    }
+
+    public static void PrintList<T> (IList<T> list)
+    {
+        foreach (var item in list)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+
+    public static void PrintListWithIndex<T> (IList<T> list)
+    {
+        Console.WriteLine($"index\tnums");
+
+        foreach (var (item, index) in list.WithIndex())
+        {
+            Console.WriteLine($"{index}\t{item}");
+        }
     }
 }
