e4c6a2c6d62acd3778594f9564f32364c9d16db1
dhiraj_slt <dhiraj.kumar@sevenlakes.com>
2022-09-16 22:22:44 +0530

    Leetcode - 1470 | Shuffle the Array

--- DSA_VS_Code/Program.cs
+++ DSA_VS_Code/Program.cs
@@ -11,13 +11,19 @@
 // RunningSumOfArray.Sum(nums);
 // Utility.PrintArray(nums);
 
-int[][] accounts = new int[][] 
-{
-    new int[] {1,5},
-    new int[] {7,3},
-    new int[] {3,5}
-};
+// int[][] accounts = new int[][] 
+// {
+//     new int[] {1,5},
+//     new int[] {7,3},
+//     new int[] {3,5}
+// };
+// Utility.Print("Richest customer wealth", RichestCustomerWealth.GetMaxWealth(accounts));
 
-Utility.Print("Richest customer wealth", RichestCustomerWealth.GetMaxWealth(accounts));
+int[] nums = new int[] {2,5,1,3,4,7};
+int n = 3;
+// Utility.PrintArray(ShuffleArray.Shuffle_1(nums, n));
+// ShuffleArray.Shuffle_2(nums, n);
+ShuffleArray.Shuffle_3(nums, n);
+Utility.PrintArray(nums);
 
 #endregion
